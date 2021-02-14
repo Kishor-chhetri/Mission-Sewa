7f44bb91801f9a0910a6345bf1a7f33a9da17a18
Kishor Chhetri <kishor.chhetri8848@gmail.com>
2021-02-14 20:50:56 +0545

    checking

--- lib/Screens/welcome_screen.dart
+++ lib/Screens/welcome_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:mission_app/Screens/event_screen.dart';
 import 'package:mission_app/components/rounded_button.dart';
-import 'login.dart';
-import 'registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'home_screen';
@@ -25,39 +24,62 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           Hero(
             tag: 'logo',
             child: Container(
-              height: 150.0,
-              width: 150.0,
+              height: 225.0,
+              width: 230.0,
               child: Image.asset('images/logo.png'),
             ),
           ),
           SizedBox(
-            height: 20,
+            height: 50,
           ),
           TypewriterAnimatedTextKit(
             text: ['Mission Sewa'],
+            repeatForever: true,
             textAlign: TextAlign.center,
             textStyle: TextStyle(
-              fontSize: 45.0,
+              fontSize: 50.0,
               fontWeight: FontWeight.w900,
             ),
           ),
+          Padding(
+            padding: EdgeInsets.only(left: 90),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                SizedBox(width: 25.0, height: 110.0),
+                Text(
+                  "For",
+                  style: TextStyle(fontSize: 25.0),
+                ),
+                SizedBox(width: 20.0, height: 100.0),
+                RotateAnimatedTextKit(
+                    repeatForever: true,
+                    text: ["YOUTH", "SOCIETY", "NATION"],
+                    textStyle: TextStyle(fontSize: 25.0, color: Colors.green),
+                    textAlign: TextAlign.start),
+              ],
+            ),
+          ),
           SizedBox(
-            height: 48.0,
+            height: 10,
           ),
           RoundedButton(
-            title: 'Log In',
-            colour: Colors.lightBlueAccent,
+            title: 'Sign In With Google',
+            colour: Color(0xffeb1555),
             onPressed: () {
-              Navigator.pushNamed(context, LoginScreen.id);
+              Navigator.pushNamed(context, EventScreen.id);
             },
           ),
-          RoundedButton(
-            title: 'Register',
-            colour: Colors.blueAccent,
-            onPressed: () {
-              Navigator.pushNamed(context, RegistrationScreen.id);
-            },
+          SizedBox(
+            height: 10,
           ),
+          Text(
+            'Login to continue',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white54,
+            ),
+          )
         ],
       ),
     ));
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mission_app/Screens/event_screen.dart';
-import 'package:mission_app/Screens/verify_email.dart';
 import 'package:mission_app/Screens/welcome_screen.dart';
-import 'package:mission_app/Screens/login.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:mission_app/Screens/registration_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,10 +17,7 @@ class MyApp extends StatelessWidget {
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
-        RegistrationScreen.id: (context) => RegistrationScreen(),
         EventScreen.id: (context) => EventScreen(),
-        VerifyEmail.id: (context) => VerifyEmail(),
       },
     );
   }
